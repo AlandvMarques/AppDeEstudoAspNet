859be1896caf1be9120c2d28621226dd6eec724f
AlandvMarques <alandvmarques@gmail.com>
2024-08-20 11:37:37 -0300

    Adicionar arquivos de projeto.

---
+++ AppMvc/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace AppMvc.Migrations
+{
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AppMvc.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(AppMvc.Models.ApplicationDbContext context)
+        {
+        }
+    }
+}
---
+++ AppMvc/Models/Aluno.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AppMvc.Models
+{
+    public class Aluno
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Nome Completo")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [MaxLength(100, ErrorMessage = "O campo Nome recebe no máximo 100 caracteres")]
+        public string Nome { get; set; }
+
+        [DisplayName("E-mail")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string CPF { get; set; }
+
+        public string Descricao { get; set; }
+
+        public DateTime DataMatricula { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
